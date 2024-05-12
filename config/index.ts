97421bf33ad658bf5aba0c372ef2cb917005e7b5
l3al3or <143728266+l3al3or@users.noreply.github.com>
2024-05-12 14:35:14 +0700

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -5,10 +5,10 @@ export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const IS_WORKFLOW = `${process.env.NEXT_PUBLIC_APP_TYPE_WORKFLOW}` === 'true'
 export const APP_INFO: AppInfo = {
   title: 'DPASANAA',
-  description: 'LINE:@DPASANAA มีโปรค่าส่ง 19 บ.',
   copyright: '',
   privacy_policy: '',
   default_language: 'en-US',
+  description: 'LINE:@DPASANAA มีโปรค่าส่ง 19 บ.',
 }
 
 export const API_PREFIX = '/api'
